5a35a83033445b701559936b1418cc7c06344819
jgowler <64489787+jgowler@users.noreply.github.com>
2025-08-31 12:26:09 +0100

    Created basic GET request Flask app
    App will accept GET requests to retrieve user information.

--- Python-Files/main.py
+++ Python-Files/main.py
@@ -1,6 +1,23 @@
-def main():
-    print("Hello from python-files!")
+from flask import Flask, jsonify
+from users import users
 
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return "Hello, this is the app"
+
+@app.route("/users", methods=["GET"])
+def userList():
+    return users
+
+@app.route("/users/<user_id>", methods=["GET"])
+def getUser(user_id):
+    key = f"user{user_id}"
+    if key in users:
+        return jsonify(users[key])
+    else:
+        return jsonify({"error": "User not found"}), 404
 
 if __name__ == "__main__":
-    main()
+    app.run()
